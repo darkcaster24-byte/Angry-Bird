6f850a7a9d26a5cf8dacde597ef707eb533519a3
darkcaster24-byte <filzim3@gmail.com>
2021-09-22 15:35:46 +0700

    first commit

---
+++ Assets/Script/OwlBird.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OwlBird : Bird
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        string tag = col.gameObject.tag;
+        if (tag == "Enemy" || tag == "Obstacle")
+        {
+            Destroy(col.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
